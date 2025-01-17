ddab5812fab5356622a397812fe5aa174873e613
Kirstyn Loftus <kirstynloftus03@gmail.com>
2025-01-16 19:05:35 -0500

    Delete PreProcessing/AE_preprocessing_pt1_v2.sql

--- PreProcessing/AE_preprocessing_pt1_v2.sql
+++
@@ -1,6 +0,0 @@
-SELECT TOP 20 * FROM ae_df
---step one: deleting columns i know aren't needed for my purposes (used FDA document for variable explanation)
-ALTER TABLE ae_df
-DROP COLUMN transmissiondateformat, transmissiondate, 
-receivedateformat, receivedate, receiptdateformat, receiptdate, fulfillexpeditecriteria, patient_summary_narrativeincludeclinical,
-primarysource_literaturereference; 
